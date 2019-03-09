5f3908fb275dd9bac5b00ce55ad1669647a92779
MouseProducedGames <>
2019-03-08 21:31:50 -0800

    Add: Player creature movement.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate rand;
 use rand::{thread_rng, Rng};
 mod creature;
 mod io;
+use crate::creature::Mobile;
 
 fn main() {
     let mut rng = thread_rng();
@@ -44,15 +45,17 @@ fn main() {
     let player_index: usize = 0;
     creatures.push(creature::Creature::new( 12, 7 ));
 
-    let (player_pos_y, player_pos_x) = creatures[player_index].get_position();
-
-    map[player_pos_y as usize][player_pos_x as usize] = 2;
+    {
+        let (player_pos_x, player_pos_y) = creatures[player_index].get_position();
+        map[player_pos_y as usize][player_pos_x as usize] = 2;
+    }
 
     io::init();
 
     let mut game_running = true;
     while game_running
     {
+        let (player_pos_x, player_pos_y) = creatures[player_index].get_position();
         io::write_map(player_pos_x, player_pos_y, &map);
         io::write_creatures(&creatures, player_index);
         io::refresh();
@@ -61,6 +64,14 @@ fn main() {
         let command = io::get_char();
         match command
         {
+            '1' => creatures[player_index].move_self(-1,  1),
+            '2' => creatures[player_index].move_self( 0,  1),
+            '3' => creatures[player_index].move_self( 1,  1),
+            '4' => creatures[player_index].move_self(-1,  0),
+            '6' => creatures[player_index].move_self( 1,  0),
+            '7' => creatures[player_index].move_self(-1, -1),
+            '8' => creatures[player_index].move_self( 0, -1),
+            '9' => creatures[player_index].move_self( 1, -1),
             'q' => game_running = false,
             _ => (),
         }
