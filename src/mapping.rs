c4b006a28d43d54698a6870e69db40dd942ce679
MouseProducedGames <>
2019-03-12 18:23:57 -0700

    Update: Mapping now has default function implementations..

--- src/mapping.rs
+++ src/mapping.rs
@@ -2,19 +2,47 @@ use super::linear::Position;
 
 pub trait Mapping
 {
-    fn bounds(&self) -> ( usize, usize);
+    fn height(&self) -> usize;
 
-    fn is_i32_in_bounds(&self, pos_x: i32, pos_y: i32) -> bool;
+    fn width(&self) -> usize;
+    
+    fn bounds(&self) -> ( usize, usize)
+    {
+        ( self.width(), self.height() )
+    }
 
-    fn is_in_bounds(&self, pos_x: usize, pos_y: usize) -> bool;
+    fn is_i32_in_bounds(&self, pos_x: i32, pos_y: i32) -> bool
+    {
+        self.is_i32_in_height( pos_y ) && self.is_i32_in_width( pos_x )
+    }
 
-    fn is_pos_in_bounds(&self, pos: Position) -> bool;
+    fn is_in_bounds(&self, pos_x: usize, pos_y: usize) -> bool
+    {
+        self.is_in_height( pos_y ) && self.is_in_width( pos_x )
+    }
 
-    fn is_i32_in_height(&self, pos_y: i32) -> bool;
+    fn is_pos_in_bounds(&self, pos: Position) -> bool
+    {
+        self.is_i32_in_bounds( pos.x, pos.y )
+    }
 
-    fn is_i32_in_width(&self, pos_x: i32) -> bool;
+    fn is_i32_in_height(&self, pos_y: i32) -> bool
+    {
+        return pos_y >= 0 && self.is_in_height( pos_y as usize)
+    }
 
-    fn is_in_height(&self, pos_y: usize) -> bool;
+    fn is_i32_in_width(&self, pos_x: i32) -> bool
+    {
+        return pos_x >= 0 && self.is_in_width( pos_x as usize)
+    }
 
-    fn is_in_width(&self, pos_x: usize) -> bool;
+    fn is_in_height(&self, pos_y: usize) -> bool
+    {
+        pos_y < self.height()
+    }
+
+    fn is_in_width(&self, pos_x: usize) -> bool
+    {
+        pos_x < self.width()
+    }
 }
--- src/visibility.rs
+++ src/visibility.rs
@@ -73,43 +73,13 @@ impl VisibilityMap
 
 impl Mapping for VisibilityMap
 {
-    fn bounds(&self) -> ( usize, usize)
+    fn height( &self ) -> usize
     {
-        ( self.width(), self.height() )
+        VisibilityMap::height( self )
     }
 
-    fn is_i32_in_bounds(&self, pos_x: i32, pos_y: i32) -> bool
+    fn width( &self ) -> usize
     {
-        self.is_i32_in_height( pos_y ) && self.is_i32_in_width( pos_x )
-    }
-
-    fn is_in_bounds(&self, pos_x: usize, pos_y: usize) -> bool
-    {
-        self.is_in_height( pos_y ) && self.is_in_width( pos_x )
-    }
-
-    fn is_pos_in_bounds(&self, pos: Position) -> bool
-    {
-        self.is_i32_in_bounds( pos.x, pos.y )
-    }
-
-    fn is_i32_in_height(&self, pos_y: i32) -> bool
-    {
-        return pos_y >= 0 && self.is_in_height( pos_y as usize)
-    }
-
-    fn is_i32_in_width(&self, pos_x: i32) -> bool
-    {
-        return pos_x >= 0 && self.is_in_width( pos_x as usize)
-    }
-
-    fn is_in_height(&self, pos_y: usize) -> bool
-    {
-        pos_y < self.height()
-    }
-
-    fn is_in_width(&self, pos_x: usize) -> bool
-    {
-        pos_x < self.width()
+        VisibilityMap::width( self )
     }
 }
