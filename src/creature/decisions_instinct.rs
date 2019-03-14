1436a876fd3c3cb5fe86e5fe8c8f7b08a254fdb0
MouseProducedGames <>
2019-03-14 00:48:23 -0700

    Seperated out common decision_instinct logic.

--- src/creature/decisions_instinct.rs
+++ src/creature/decisions_instinct.rs
@@ -8,7 +8,7 @@ Documentation:
 
 // External includes
 extern crate rand;
-use rand::Rngx;
+use rand::Rng;
 use rand::rngs::ThreadRng;
 
 // Internal includes
@@ -20,9 +20,14 @@ pub fn make_decision_attack( emotion: LoveFearHate, rel_str: f32, rnd: &mut Thre
         true => {
             let hate_sub_fear = emotion.calc_hate( rel_str ) - emotion.calc_fear( rel_str );
             let zero_to_one = ( hate_sub_fear * 0.5 ) + 0.5;
-            let attack_chance = zero_to_one.powf( 2.0 );
-            attack_chance >= rnd.gen_range( 0.0, 1.0 )
+            calc_response_zero_one( zero_to_one, rnd )
         },
         false => false,
     }
+}
+
+fn calc_response_zero_one( value: f32, rnd: &mut ThreadRng ) -> bool
+{    
+    let chance = value.powf( 2.0 );
+    chance >= rnd.gen_range( 0.0, 1.0 )
 }
