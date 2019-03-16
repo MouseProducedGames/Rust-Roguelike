221d4424fdfebd97e0847733046d80e4b7c93036
MouseProducedGames <>
2019-03-15 23:46:32 -0700

    Adding: Template meta-programming map-drawing routine invovling recursive trait typing.

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -4,9 +4,11 @@
 pub mod line_of_sight;
 pub mod mapping;
 pub mod tilemap;
+pub mod tileroom;
 pub mod tiletype;
 pub mod visibility_map;
 pub use line_of_sight::calculate_visibility;
 pub use mapping::Mapping;
+pub use tileroom::{ TiledArea, TiledAreaFilter };
 pub use tilemap::Tilemap;
 pub use visibility_map::{ VisibilityMap, VisibilityType };
---
+++ src/world/tileroom.rs
@@ -0,0 +1,201 @@
+/** Copyright (C) 2019 MouseProducedGames
+
+See license in the LICENSE file
+
+Documentation:
+
+**/
+
+// External includes
+
+// Internal includes
+use super::super::tiled_shapes_2d::TiledShape2D;
+use super::{ Mapping, Tilemap };
+
+pub struct TiledAreaFilter<'a>
+{
+    area: &'a mut TiledArea,
+    shape_filter: &'a mut TiledShape2D,
+}
+
+impl<'a> TiledAreaFilter<'a>
+{
+    pub fn new( area: &'a mut TiledArea, shape_filter: &'a mut TiledShape2D ) -> Self
+    {
+        Self { area: area, shape_filter: shape_filter }
+    }
+}
+
+impl Mapping for TiledArea
+{
+    fn height( &self ) -> usize
+    {
+        TiledArea::height( self ) as usize
+    }
+
+    fn width( &self ) -> usize
+    {
+        TiledArea::width( self ) as usize
+    }
+}
+
+pub trait TiledArea
+{
+    fn circumference( &self ) -> u32;
+
+    fn height( &self ) -> u32;
+
+    fn iter_circumference( &self, iter_index: &mut u32 ) -> Option< ( u32, u32 ) >;
+    
+    fn iter_surface_area( &self, iter_index: &mut u32 ) -> Option< ( u32, u32 ) >;
+    
+    fn surface_area( &self ) -> u32;
+
+    fn value( &self, x: u32, y: u32 ) -> u32;
+
+    fn value_mut( &mut self, x: u32, y: u32 ) -> &mut u32;
+
+    fn width( &self ) -> u32;
+}
+
+impl TiledArea for Tilemap
+{
+    fn circumference( &self ) -> u32
+    {
+        // Should be optimized: TWo gets, two adds, and a return.
+        let half = self.height() + self.width();
+        ( half + half ) as u32
+    }
+
+    fn height( &self ) -> u32
+    {
+        self.height() as u32
+    }
+
+    fn iter_circumference( &self, iter_index: &mut u32 ) -> Option< ( u32, u32 ) >
+    {
+        let ( width, height ) = ( self.width() as u32, self.height() as u32 );
+        let index = *iter_index;
+        *iter_index += 1;
+        if index < width
+        {
+            Some( ( index, height ) )
+        }
+        else if index < ( width + height )
+        {
+            Some( ( width, height + ( index - width ) ) )
+        }
+        else if index < ( width + height + width )
+        {
+            let temp = index - ( width + height );
+            Some( ( temp, height ) )
+        }
+        else if index < ( width + height + width + height )
+        {
+            let temp = index - ( width + height + width );
+            Some( ( 0, temp ) )
+        }
+        else { None }
+    }
+    
+    fn iter_surface_area( &self, iter_index: &mut u32 ) -> Option< ( u32, u32 ) >
+    {
+        let ( width, height ) = ( self.width() as u32, self.height() as u32 );
+        let index = *iter_index;
+        *iter_index += 1;
+        let x = index % width;
+        let y = index / width;
+        if y == height
+        {
+            return None;
+        }
+        
+        Some( ( ( x ), ( y ) ) )
+    }
+    
+    fn surface_area( &self ) -> u32
+    {
+        ( self.height() * self.width() ) as u32
+    }
+
+    fn value( &self, x: u32, y: u32 ) -> u32
+    {
+        self.tile_type( x as usize, y as usize )
+    }
+
+    fn value_mut( &mut self, x: u32, y: u32 ) -> &mut u32
+    {
+        self.tile_type_mut( x as usize, y as usize )
+    }
+
+    fn width( &self ) -> u32
+    {
+        self.width() as u32
+    }
+}
+
+impl<'a> TiledArea for TiledAreaFilter<'a>
+{
+    fn circumference( &self ) -> u32
+    {
+        self.shape_filter.circumference()
+    }
+
+    fn height( &self ) -> u32
+    {
+        self.area.height()
+    }
+
+    fn iter_circumference( &self, iter_index: &mut u32 ) -> Option< ( u32, u32 ) >
+    {
+        self.shape_filter.iter_circumference( iter_index )
+    }
+    
+    fn iter_surface_area( &self, iter_index: &mut u32 ) -> Option< ( u32, u32 ) >
+    {
+        self.shape_filter.iter_surface_area( iter_index )
+    }
+    
+    fn surface_area( &self ) -> u32
+    {    
+        self.shape_filter.surface_area()
+    }
+
+    fn value( &self, x: u32, y: u32 ) -> u32
+    {
+        self.area.value( x, y )
+    }
+
+    fn value_mut( &mut self, x: u32, y: u32 ) -> &mut u32
+    {
+        self.area.value_mut( x, y )
+    }
+
+    fn width( &self ) -> u32
+    {
+        self.area.width()
+    }
+}
+
+impl TiledShape2D for TiledArea
+{
+    fn circumference( &self ) -> u32
+    {
+        TiledArea::circumference( self )
+    }
+
+    fn iter_circumference( &self, iter_index: &mut u32 ) -> Option< ( u32, u32 ) >
+    {
+        TiledArea::iter_circumference( self, iter_index )
+    }
+
+    fn iter_surface_area( &self, iter_index: &mut u32 ) -> Option< ( u32, u32 ) >
+    {
+        TiledArea::iter_surface_area( self, iter_index )
+    }
+
+    fn surface_area( &self ) -> u32
+    {
+        TiledArea::surface_area( self )
+    }
+}
