ae8b2f9f9e2592e98bbd8882ad5f769fefa355fa
MouseProducedGames <>
2019-03-19 20:46:15 -0700

    Fix: Remaining merge errors.

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,6 @@ use faction::Faction;
 use game_state::GameState;
 use io::Window;
 use rrl_math::{ Bounds,  Position };
-use stats::Health;
 use world::Tilemap;
 
 fn main() {
@@ -80,7 +79,6 @@ fn main() {
     world.register::< CreatureLogicWander >();
     world.register::< CreatureStats >();
     world.register::< Faction >();
-    world.register::< Health >();
     world.register::< PlayerMarker >();
     world.register::< Position >();
     world.register::< SightRange >();
