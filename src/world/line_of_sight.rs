686acd1e663f51e3e7ff93cfe63cbbf0ebdb4b0b
MouseProducedGames <>
2019-04-01 15:16:26 -0700

    Update: Optimized line of sight calculations.

--- src/world/line_of_sight.rs
+++ src/world/line_of_sight.rs
@@ -25,7 +25,7 @@ pub fn calculate_visibility(
     }
 
     for to_y in -sight_range..=sight_range {
-        for to_x in -sight_range..=sight_range {
+        for to_x in (-sight_range..=sight_range).step_by((sight_range * 2) as usize) {
             let disp = Displacement::new(to_x, to_y);
 
             let to_pos = pos + disp;
@@ -58,6 +58,78 @@ pub fn calculate_visibility(
                 move_pos_x += disp_move_x;
                 move_pos_y += disp_move_y;
             }
+        }
+    }
+
+    for to_x in -sight_range..=sight_range {
+        let to_y = -sight_range;
+        let disp = Displacement::new(to_x, to_y);
+
+        let to_pos = pos + disp;
+        let disp_x = f64::from(to_pos.x - pos.x);
+        let disp_y = f64::from(to_pos.y - pos.y);
+        let dist = ((disp_x * disp_x) + (disp_y * disp_y)).sqrt();
+        let disp_norm_x = disp_x / dist;
+        let disp_norm_y = disp_y / dist;
+        let disp_move_x = disp_norm_x / 2.0;
+        let disp_move_y = disp_norm_y / 2.0;
+        let mut move_pos_x: f64 = 0.0;
+        let mut move_pos_y: f64 = 0.0;
+        let mut check_pos_x = f64::from(pos.x) + 0.5;
+        let mut check_pos_y = f64::from(pos.y) + 0.5;
+        while ((move_pos_x * move_pos_x) + (move_pos_y * move_pos_y)) <= sight_range_sqr {
+            let check_pos = Position::new(check_pos_x as i32, check_pos_y as i32);
+            if map.is_pos_in_bounds(check_pos) == false {
+                break;
+            }
+            *visibility.value_mut(check_pos.x as u32, check_pos.y as u32) =
+                VisibilityType::Visible;
+            if map.transparent_pos(check_pos) == false
+                && ((check_pos.x != pos.x) || (check_pos.y != pos.y))
+            {
+                break;
+            }
+
+            check_pos_x += disp_move_x;
+            check_pos_y += disp_move_y;
+            move_pos_x += disp_move_x;
+            move_pos_y += disp_move_y;
+        }
+    }
+
+    for to_x in -sight_range..=sight_range {
+        let to_y = sight_range;
+        let disp = Displacement::new(to_x, to_y);
+
+        let to_pos = pos + disp;
+        let disp_x = f64::from(to_pos.x - pos.x);
+        let disp_y = f64::from(to_pos.y - pos.y);
+        let dist = ((disp_x * disp_x) + (disp_y * disp_y)).sqrt();
+        let disp_norm_x = disp_x / dist;
+        let disp_norm_y = disp_y / dist;
+        let disp_move_x = disp_norm_x / 2.0;
+        let disp_move_y = disp_norm_y / 2.0;
+        let mut move_pos_x: f64 = 0.0;
+        let mut move_pos_y: f64 = 0.0;
+        let mut check_pos_x = f64::from(pos.x) + 0.5;
+        let mut check_pos_y = f64::from(pos.y) + 0.5;
+        while ((move_pos_x * move_pos_x) + (move_pos_y * move_pos_y)) <= sight_range_sqr {
+            let check_pos = Position::new(check_pos_x as i32, check_pos_y as i32);
+            if map.is_pos_in_bounds(check_pos) == false {
+                break;
+            }
+            *visibility.value_mut(check_pos.x as u32, check_pos.y as u32) =
+                VisibilityType::Visible;
+            if map.transparent_pos(check_pos) == false
+                && ((check_pos.x != pos.x) || (check_pos.y != pos.y))
+            {
+                break;
+            }
+
+            check_pos_x += disp_move_x;
+            check_pos_y += disp_move_y;
+            move_pos_x += disp_move_x;
+            move_pos_y += disp_move_y;
         }
     }
 }
