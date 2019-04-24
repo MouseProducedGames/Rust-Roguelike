5d53874c0d044e3d3fb6bc27fdb6a5f27b8df957
MouseProducedGames <>
2019-04-24 10:42:50 -0700

    Move: SuccessResult to its own file.

--- src/dice/mod.rs
+++ src/dice/mod.rs
@@ -10,9 +10,11 @@ use rand::{thread_rng, Rng};
 use rust_dice::{Die, Roll, RollSet};
 
 // Standard includes.
-use std::ops::Deref;
 
 // Internal includes.
+mod success_result;
+pub use success_result::SuccessResult;
+
 use crate::game::combat::{AttackValue, DefenceValue};
 use crate::rrl_math::Displacement;
 
@@ -39,39 +41,6 @@ pub fn get_random_move() -> Displacement {
 
 fn random_wander_command() -> u32 {
     thread_rng().gen_range(1, 10)
-}
-
-#[derive(Copy, Clone, Eq, PartialEq)]
-pub struct SuccessResult(bool, i32);
-
-impl SuccessResult {
-    pub fn new(roll: i32) -> Self {
-        SuccessResult(roll > 10, roll)
-    }
-
-    pub fn is_failure(self) -> bool {
-        self.is_success() == false
-    }
-
-    pub fn is_success(self) -> bool {
-        self.0
-    }
-
-    pub fn margin_of_success(self) -> i32 {
-        self.roll() - 10
-    }
-
-    pub fn roll(self) -> i32 {
-        self.1
-    }
-}
-
-impl Deref for SuccessResult {
-    type Target = bool;
-
-    fn deref(&self) -> &bool {
-        &self.0
-    }
 }
 
 pub fn roll_attack(attack_value: AttackValue, defence_value: DefenceValue) -> SuccessResult {
---
+++ src/dice/success_result.rs
@@ -0,0 +1,46 @@
+/** Copyright (C) 2019 MouseProducedGames
+
+See license in the LICENSE file
+
+Documentation:
+
+ **/
+// External includes.
+
+// Standard includes.
+use std::ops::Deref;
+
+// Internal includes.
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct SuccessResult(bool, i32);
+
+impl SuccessResult {
+    pub fn new(roll: i32) -> Self {
+        SuccessResult(roll > 10, roll)
+    }
+
+    pub fn is_failure(self) -> bool {
+        self.is_success() == false
+    }
+
+    pub fn is_success(self) -> bool {
+        self.0
+    }
+
+    pub fn margin_of_success(self) -> i32 {
+        self.roll() - 10
+    }
+
+    pub fn roll(self) -> i32 {
+        self.1
+    }
+}
+
+impl Deref for SuccessResult {
+    type Target = bool;
+
+    fn deref(&self) -> &bool {
+        &self.0
+    }
+}
