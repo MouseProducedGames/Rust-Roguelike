10bd0f33afeb5922577a03e23386c3ace38e15f6
MouseProducedGames <>
2019-04-30 11:21:24 -0700

    Rename: themes_init_screen.rs -> theme_init_screen.rs.

--- src/themes/mod.rs
+++ src/themes/mod.rs
@@ -12,9 +12,9 @@ Documentation:
 // Internal includes.
 mod theme;
 mod theme_helper;
+mod theme_init_screen;
 mod theme_lookup;
-mod themes_init_screen;
 pub use theme::Theme;
 pub use theme_helper::ThemeHelper;
+pub use theme_init_screen::ThemeInitScreen;
 pub use theme_lookup::ThemeLookup;
-pub use themes_init_screen::ThemeInitScreen;
