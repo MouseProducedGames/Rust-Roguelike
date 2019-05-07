83c1448768a263d78ed3b2bf4777cd43791b75aa
MouseProducedGames <>
2019-05-07 15:01:30 -0700

    Add: LeveledWeaponFactory for generating weapons of varying quality.

---
+++ src/items/weapons/factories/specific/leveled_weapon_factory.rs
@@ -0,0 +1,124 @@
+/** Copyright (C) 2019 MouseProducedGames
+
+See license in the LICENSE file
+
+Documentation:
+
+**/
+// External includes.
+use rand::{thread_rng, Rng};
+use specs::{Entity, World};
+
+// Standard includes.
+use std::default::Default;
+use std::sync::Arc;
+
+// Internal includes.
+use super::UnmodifiedWeaponFactory;
+use crate::data_types::Name;
+use crate::game::combat::AttackValue;
+use crate::game::points::{BuildPoints, CostsBuildPoints, CostsCurrency, CurrencyValue};
+use crate::items::weapons::factories::traits::{FineWeaponProcessor, RustyWeaponProcessor};
+use crate::items::weapons::factories::{WeaponFactory, WeaponProcessor};
+use crate::items::weapons::Weapon;
+
+#[derive(Clone)]
+pub struct LeveledWeaponFactory {
+    level: i32,
+    weapon_factories: Arc<Vec<Arc<dyn WeaponFactory>>>,
+    quality_processors: Arc<Vec<Arc<dyn WeaponProcessor>>>,
+    flaw_processors: Arc<Vec<Arc<dyn WeaponProcessor>>>,
+}
+
+impl LeveledWeaponFactory {
+    fn new(level: i32) -> Self {
+        Self {
+            level,
+            weapon_factories: Arc::new(vec![Arc::new(UnmodifiedWeaponFactory::default())]),
+            quality_processors: Arc::new(vec![Arc::new(FineWeaponProcessor::default())]),
+            flaw_processors: Arc::new(vec![Arc::new(RustyWeaponProcessor::default())]),
+        }
+    }
+}
+
+impl WeaponFactory for LeveledWeaponFactory {
+    fn create(&self, world: &mut World) -> Entity {
+        let item_entity;
+        {
+            let index = thread_rng().gen_range(0, self.weapon_factories.len());
+            item_entity = self.weapon_factories[index].create(world);
+        }
+
+        if self.level > 0 {
+            let mut level = self.level;
+            let mut quality_indices: Vec<usize> = (0..self.quality_processors.len()).collect();
+            while level > 0 && quality_indices.is_empty() == false {
+                let quality_indices_index = thread_rng().gen_range(0, quality_indices.len());
+                let quality_index = quality_indices[quality_indices_index];
+                self.quality_processors[quality_index].process(world, item_entity);
+
+                quality_indices.remove(quality_indices_index);
+                level -= 1;
+            }
+
+            if level > 0 {
+                {
+                    let mut weapon_storage = world.write_storage::<Weapon>();
+                    let weapon = weapon_storage.get_mut(item_entity).unwrap();
+                    *weapon.attack_value_mut() += AttackValue::from(level);
+                }
+
+                {
+                    let mut name_storage = world.write_storage::<Name>();
+                    let name = name_storage.get_mut(item_entity).unwrap();
+                    name.insert_str(0, " ");
+                    name.insert_str(0, &level.to_string());
+                    name.insert_str(0, "+");
+                }
+            }
+        } else if self.level < 0 {
+            let mut level = self.level;
+            let mut flaw_indices: Vec<usize> = (0..self.flaw_processors.len()).collect();
+            while level < 0 && flaw_indices.is_empty() == false {
+                let flaw_indices_index = thread_rng().gen_range(0, flaw_indices.len());
+                let flaw_index = flaw_indices[flaw_indices_index];
+                self.flaw_processors[flaw_index].process(world, item_entity);
+
+                flaw_indices.remove(flaw_indices_index);
+                level += 1;
+            }
+
+            if level < 0 {
+                {
+                    let mut weapon_storage = world.write_storage::<Weapon>();
+                    let weapon = weapon_storage.get_mut(item_entity).unwrap();
+                    *weapon.attack_value_mut() += AttackValue::from(level);
+                }
+
+                {
+                    let mut name_storage = world.write_storage::<Name>();
+                    let name = name_storage.get_mut(item_entity).unwrap();
+                    name.insert_str(0, " ");
+                    name.insert_str(0, &level.to_string());
+                }
+            }
+        }
+
+        {
+            let weapon = *world.read_storage::<Weapon>().get(item_entity).unwrap();
+            let build_points = weapon.build_points_total(world);
+            let currency_value = weapon.currency_total(world);
+
+            *world
+                .write_storage::<BuildPoints>()
+                .get_mut(item_entity)
+                .unwrap() = build_points;
+            *world
+                .write_storage::<CurrencyValue>()
+                .get_mut(item_entity)
+                .unwrap() = currency_value;
+        }
+
+        item_entity
+    }
+}
--- src/items/weapons/factories/specific/mod.rs
+++ src/items/weapons/factories/specific/mod.rs
@@ -12,9 +12,11 @@ Documentation:
 // Internal includes.
 pub mod axes;
 pub mod body_parts;
+mod leveled_weapon_factory;
 pub mod maces;
 pub mod shields;
 pub mod spears;
 pub mod swords;
 mod unmodified_weapon_factory;
+pub use leveled_weapon_factory::LeveledWeaponFactory;
 pub use unmodified_weapon_factory::UnmodifiedWeaponFactory;
