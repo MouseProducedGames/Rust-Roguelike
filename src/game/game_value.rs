84301e7d515e2851664fe59646def3fa71dde206
MouseProducedGames <>
2019-05-13 13:03:59 -0700

    Update: GameValue is now templated from TemplateGameValue.

--- src/game/game_value.rs
+++ src/game/game_value.rs
@@ -8,156 +8,45 @@ Documentation:
 // External includes.
 
 // Standard includes.
-use std::cmp::{Ord, Ordering, PartialOrd};
+use super::TemplateGameValue;
 use std::convert::From;
-use std::hash::{Hash, Hasher};
-use std::marker::PhantomData;
-use std::ops::{Add, AddAssign, Sub, SubAssign};
 
 // Internal includes.
 
-#[derive(Copy, Clone, Default)]
-pub struct GameValue<T>(i32, PhantomData<T>)
-where
-    T: Copy + Clone + Default;
+pub type GameValue<TMarker> = TemplateGameValue<i32, TMarker>;
 
-impl<T> GameValue<T>
+impl<TMarker> From<i32> for GameValue<TMarker>
 where
-    T: Copy + Clone + Default,
+    TMarker: Copy + Clone + Default,
 {
-    fn new(value: i32) -> Self {
-        Self(value, PhantomData)
+    fn from(other: i32) -> Self {
+        Self::new(other)
     }
 }
 
-impl<T> Add for GameValue<T>
+impl<TMarker> From<&i32> for GameValue<TMarker>
 where
-    T: Copy + Clone + Default,
+    TMarker: Copy + Clone + Default,
 {
-    type Output = GameValue<T>;
-
-    fn add(self, other: GameValue<T>) -> Self {
-        GameValue::from(self.0 + other.0)
+    fn from(other: &i32) -> Self {
+        Self::from(*other)
     }
 }
 
-impl<T> AddAssign for GameValue<T>
+impl<TMarker> From<GameValue<TMarker>> for i32
 where
-    T: Copy + Clone + Default,
+    TMarker: Copy + Clone + Default,
 {
-    fn add_assign(&mut self, other: GameValue<T>) {
-        self.0 += other.0;
+    fn from(other: GameValue<TMarker>) -> Self {
+        other.raw()
     }
 }
 
-impl<T> Eq for GameValue<T> where T: Copy + Clone + Default {}
-
-impl<T> From<i32> for GameValue<T>
+impl<TMarker> From<&GameValue<TMarker>> for i32
 where
-    T: Copy + Clone + Default,
+    TMarker: Copy + Clone + Default,
 {
-    fn from(value: i32) -> Self {
-        Self::new(value)
-    }
-}
-
-impl<T> From<&i32> for GameValue<T>
-where
-    T: Copy + Clone + Default,
-{
-    fn from(value: &i32) -> Self {
-        Self::from(*value)
-    }
-}
-
-impl<T> From<GameValue<T>> for i32
-where
-    T: Copy + Clone + Default,
-{
-    fn from(value: GameValue<T>) -> Self {
-        value.0
-    }
-}
-
-impl<T> From<&GameValue<T>> for i32
-where
-    T: Copy + Clone + Default,
-{
-    fn from(value: &GameValue<T>) -> Self {
-        Self::from(*value)
-    }
-}
-
-impl<T> Hash for GameValue<T>
-where
-    T: Copy + Clone + Default,
-{
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.0.hash(state);
-    }
-}
-
-impl<T> Ord for GameValue<T>
-where
-    T: Copy + Clone + Default,
-{
-    fn cmp(&self, other: &GameValue<T>) -> Ordering {
-        self.0.cmp(&other.0)
-    }
-}
-
-impl<T> PartialEq for GameValue<T>
-where
-    T: Copy + Clone + Default,
-{
-    fn eq(&self, other: &GameValue<T>) -> bool {
-        self.0 == other.0
-    }
-}
-
-impl<T> PartialEq<i32> for GameValue<T>
-where
-    T: Copy + Clone + Default,
-{
-    fn eq(&self, other: &i32) -> bool {
-        self.0 == *other
-    }
-}
-
-impl<T> PartialOrd for GameValue<T>
-where
-    T: Copy + Clone + Default,
-{
-    fn partial_cmp(&self, other: &GameValue<T>) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<T> PartialOrd<i32> for GameValue<T>
-where
-    T: Copy + Clone + Default,
-{
-    fn partial_cmp(&self, other: &i32) -> Option<Ordering> {
-        Some(self.cmp(&GameValue::<T>::from(other)))
-    }
-}
-
-impl<T> Sub for GameValue<T>
-where
-    T: Copy + Clone + Default,
-{
-    type Output = GameValue<T>;
-
-    fn sub(self, other: GameValue<T>) -> Self {
-        GameValue::from(self.0 - other.0)
-    }
-}
-
-impl<T> SubAssign for GameValue<T>
-where
-    T: Copy + Clone + Default,
-{
-    fn sub_assign(&mut self, other: GameValue<T>) {
-        self.0 -= other.0;
+    fn from(other: &GameValue<TMarker>) -> Self {
+        Self::from(*other)
     }
 }
--- src/game/template_game_value.rs
+++ src/game/template_game_value.rs
@@ -18,26 +18,62 @@ use std::ops::{Add, AddAssign, Sub, SubAssign};
 #[derive(Copy, Clone, Default)]
 pub struct TemplateGameValue<TData, TMarker>(TData, PhantomData<TMarker>)
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
     TMarker: Copy + Clone + Default;
 
 impl<TData, TMarker> TemplateGameValue<TData, TMarker>
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
     TMarker: Copy + Clone + Default,
 {
-    fn new(value: TData) -> Self {
+    pub fn new(value: TData) -> Self {
         Self(value, PhantomData)
     }
-    
-    fn raw(self) -> TData {
+
+    pub fn raw(self) -> TData {
         self.0
     }
 }
 
 impl<TData, TMarker> Add for TemplateGameValue<TData, TMarker>
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
     TMarker: Copy + Clone + Default,
 {
     type Output = TemplateGameValue<TData, TMarker>;
@@ -49,7 +85,19 @@ where
 
 impl<TData, TMarker> AddAssign for TemplateGameValue<TData, TMarker>
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
     TMarker: Copy + Clone + Default,
 {
     fn add_assign(&mut self, other: TemplateGameValue<TData, TMarker>) {
@@ -59,13 +107,38 @@ where
 
 impl<TData, TMarker> Eq for TemplateGameValue<TData, TMarker>
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
-    TMarker: Copy + Clone + Default
-{}
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
+    TMarker: Copy + Clone + Default,
+{
+}
 
 impl<TData, TMarker> Hash for TemplateGameValue<TData, TMarker>
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
     TMarker: Copy + Clone + Default,
 {
     fn hash<H: Hasher>(&self, state: &mut H) {
@@ -75,7 +148,19 @@ where
 
 impl<TData, TMarker> Ord for TemplateGameValue<TData, TMarker>
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
     TMarker: Copy + Clone + Default,
 {
     fn cmp(&self, other: &TemplateGameValue<TData, TMarker>) -> Ordering {
@@ -85,7 +170,19 @@ where
 
 impl<TData, TMarker> PartialEq for TemplateGameValue<TData, TMarker>
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
     TMarker: Copy + Clone + Default,
 {
     fn eq(&self, other: &TemplateGameValue<TData, TMarker>) -> bool {
@@ -95,7 +192,19 @@ where
 
 impl<TData, TMarker> PartialEq<TData> for TemplateGameValue<TData, TMarker>
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
     TMarker: Copy + Clone + Default,
 {
     fn eq(&self, other: &TData) -> bool {
@@ -105,7 +214,19 @@ where
 
 impl<TData, TMarker> PartialOrd for TemplateGameValue<TData, TMarker>
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
     TMarker: Copy + Clone + Default,
 {
     fn partial_cmp(&self, other: &TemplateGameValue<TData, TMarker>) -> Option<Ordering> {
@@ -115,7 +236,19 @@ where
 
 impl<TData, TMarker> PartialOrd<TData> for TemplateGameValue<TData, TMarker>
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
     TMarker: Copy + Clone + Default,
 {
     fn partial_cmp(&self, other: &TData) -> Option<Ordering> {
@@ -125,7 +258,19 @@ where
 
 impl<TData, TMarker> Sub for TemplateGameValue<TData, TMarker>
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
     TMarker: Copy + Clone + Default,
 {
     type Output = TemplateGameValue<TData, TMarker>;
@@ -137,7 +282,19 @@ where
 
 impl<TData, TMarker> SubAssign for TemplateGameValue<TData, TMarker>
 where
-    TData: Add<Output = TData> + AddAssign + Eq + Copy + Clone + Default + Hash + Ord + PartialEq + PartialOrd + Sized + Sub<Output = TData> + SubAssign,
+    TData: Add<Output = TData>
+        + AddAssign
+        + Eq
+        + Copy
+        + Clone
+        + Default
+        + Hash
+        + Ord
+        + PartialEq
+        + PartialOrd
+        + Sized
+        + Sub<Output = TData>
+        + SubAssign,
     TMarker: Copy + Clone + Default,
 {
     fn sub_assign(&mut self, other: TemplateGameValue<TData, TMarker>) {
